c435db96c706f5f4807a154fb0a382f5ade51090
jasmaa <jasonmaa3955@gmail.com>
2018-12-27 18:41:30 -0500

    UI working in VR

---
+++ Assets/Scripts/UI/UserList.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserList : MonoBehaviour {
+
+	public GameObject userWidgetTemplate;
+	public GameObject widgetRoot;
+
+	private List<GameObject> userWidgets;
+
+
+	void Start(){
+		userWidgets = new List<GameObject> ();
+	}
+
+	void Update(){
+		UpdateUsers ();
+	}
+
+	void UpdateUsers(){
+
+		// empty old
+		foreach (GameObject userWidget in userWidgets) {
+			Destroy (userWidget);
+		}
+		userWidgets.Clear ();
+
+		// re-add new users
+		int count = 0;
+		foreach(User user in GameManager.instance.client.pb.users.Values){
+			GameObject userWidget = Instantiate(userWidgetTemplate, widgetRoot.transform);
+			userWidget.transform.Find ("UserName").GetComponent<Text>().text = user.name;
+			userWidget.transform.Find ("UserScore").GetComponent<Text>().text = user.score+"";
+			userWidget.transform.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (0, 0.3f - 0.11f * count);
+
+			userWidgets.Add (userWidget);
+
+			count++;
+		}
+		
+	}
+}
--- readme.md
+++ readme.md
@@ -2,6 +2,11 @@
 
 WIP PB port for Oculus
 
+## Controls
+  - B/Y for next question
+  - A/X while grabbing buzzer to buzz
+  - Swipe up/down while pointing to close/open settings
+
 ## Acknowledgements
   - [WebSocketSharp](https://github.com/sta/websocket-sharp)
   - [SimpleJSON](https://github.com/Bunny83/SimpleJSON)
